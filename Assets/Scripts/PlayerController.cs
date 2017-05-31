969e2b5f8bdb9c7c0836dc2547e52ac34a00b716
koedijk <17798@ma-web.nl>
2017-05-31 16:42:19 +0200

    First Setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5f;
+    private Transform graphics;
+    [SerializeField]
+    private SkeletonAnimation[] skeletonAnimations;
+    private Rigidbody2D rigid;
+
+    private float x;
+
+    void Awake()
+    {
+        graphics = transform;
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal") * speed;
+        if (x > 0.2)
+        {
+            graphics.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if(x < 0.2)
+        {
+            graphics.localRotation = Quaternion.Euler(0,180,0);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(x,rigid.velocity.y);
+    }
+
+}
